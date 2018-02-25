4a4b2179578c5b40b133b42360fe9c20bdf4fb7d
Larry Botha <larry@fixate.it>
2018-02-25 10:56:56 +0100

    add notes on real-time communication in feathers

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const express = require('@feathersjs/express');
 const feathers = require('@feathersjs/feathers');
-const memory = require('feathers-memory');
+const express = require('@feathersjs/express');
 const socketio = require('@feathersjs/socketio');
+const memory = require('feathers-memory');
+
+// Feathers services automatically send created, udpated, patched, and
+// removed events when a create, update, patch, or remove service method
+// returns.
+// These messages are also published to connected clients so they can
+// react accordingly.
+
+// REST does not support real-time, so we need a transport that does.
+// SocketIO and Primus have official transports in FeathersJS.
+// Real-time communicaiton requires bi-directional communiation.
+
+// The feathers team actually recommends using socket transpsorts since
+// they allow service method calling, and generally perform better than REST
+// calls.
