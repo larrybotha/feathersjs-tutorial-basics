5a8b084e67d554ac88dfbc58299b29d075adc4cb
Larry Botha <larry@fixate.it>
2018-02-25 10:57:21 +0100

    create the express app

--- app.js
+++ app.js
@@ -16,3 +16,33 @@ const memory = require('feathers-memory');
 // The feathers team actually recommends using socket transpsorts since
 // they allow service method calling, and generally perform better than REST
 // calls.
+
+// create a feathers app
+const app = express(feathers());
+
+// parse json requests
+app.use(express.json());
+// parse url queries with nested data
+app.use(express.urlencoded({extended: true}));
+// configure express to use REST as the protocol
+app.configure(express.rest());
+app.use(
+  'messages',
+  memory({
+    paginate: {
+      default: 10,
+      max: 25,
+    },
+  })
+);
+
+// add error handling
+app.use(express.errorHandler());
+
+// start the server
+// Making a request to this opens a connection which needs to manually closed
+const server = app.listen(3030);
+
+server.on('listening', () =>
+  console.log('Feathers API started at localhost:3030')
+);
